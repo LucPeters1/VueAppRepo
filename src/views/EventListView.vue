4797d2fac832e53f567bd7aec59f0b84f3f198ea
Luc Peters <88270637+LucPeters1@users.noreply.github.com>
2023-11-15 16:05:47 +0100

    first commit

---
+++ src/views/EventListView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import EventCard from '../components/EventCard.vue';
+import { onMounted, ref } from 'vue';
+import EventSerivce from '../services/EventService.js';
+
+const events = ref(null)
+
+onMounted(() => {
+  EventSerivce.getEvents()
+  .then((response) => {
+    events.value = response.data
+  
+  }).catch((error) => {
+    console.log(error)
+  })
+})
+</script>
+
+<template>
+  <h1>Events for good</h1>
+  <div class="events">
+  <EventCard v-for="event in events" :key="event.id" :event="event"/>
+</div>
+</template>
+
+<style scroped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
